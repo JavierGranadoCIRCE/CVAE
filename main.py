38bc548869228769f153be4139c73b68e30e03f4
Javier Granado <601354@unizar.es>
2025-01-21 11:45:43 +0100

    pequeños cambios para que empiece a funcionar

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import tensorflow as tf
 from Conditional_VAE import generate_data, conditional_vae
 from downstream_task import *
 from configuration import hyperparams, train_SOC_values_cases
-<<<<<<< HEAD
+
 ####################################### Prueba Commit 17012025_7
 
->>>>>>> CVAE/main
+
 
 # --- Set Seed for Reproducibility ---
 
