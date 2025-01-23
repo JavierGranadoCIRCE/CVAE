b25e6f30e310c683bf5a13fab96b6d66882aa74c
Javier Granado <601354@unizar.es>
2025-01-23 13:15:10 +0100

    Prueba Commit 23012025_7

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from Conditional_VAE import generate_data, conditional_vae
 from downstream_task import *
 from configuration import hyperparams, train_SOC_values_cases
 
-####################################### Prueba Commit 17012025_7
+####################################### Prueba Commit 23012025_7
+
 
 
 
